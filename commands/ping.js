e0ffcd6d5c1e5fa3d33fb1a811fa8da54ba5399a
Mazz <ma15fo43@gmail.com>
2020-08-03 21:51:23 +0200

    Cleaned code

--- commands/ping.js
+++ commands/ping.js
@@ -7,36 +7,31 @@ const moment = require('moment');
 const fetch = require('node-fetch');
 client.commands = new Discord.Collection(); 
 const caca = JSON.parse(fs.readFileSync("./prefix.json", "utf8"));
-const pref = caca["prefix"]
+const pref = caca["prefix"];
 const neezay = JSON.parse(fs.readFileSync("./token.json", "utf8"));
-const token = neezay["token"]
+const token = neezay["token"];
+
 client.login(token);
 
-
-
-    client.on("message", async message => {
-        const prefix = pref + "ping";
-        if (message.author.bot) return;
-        if (!message.guild) return;
-        if (!message.content.startsWith(prefix)) return;
+client.on("message", async message => {
+    const prefix = pref + "ping";
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
       
-        let début = Date.now();
+    let début = Date.now();
       
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const cmd = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
 
-        if (cmd === "") {
-            if (message.author.id != "413423609122390016") return message.channel.send("**Seuls les créateurs du bots peuvent exercer cette commande.**");
-            const msg = await message.channel.send(`
-            PingGing...`);
-            msg.edit(`Pong 🏓 J'ai une latence de ${Date.now() - début} ms`);
-        }   
-      });
+    if (cmd === "") {
+        if (message.author.id != "413423609122390016") return message.channel.send("**Seuls les créateurs du bots peuvent exercer cette commande.**");
+        const msg = await message.channel.send(`
+        PingGing...`);
+        msg.edit(`Pong 🏓 J'ai une latence de ${Date.now() - début} ms`);
+    }   
+});
       
-
-
-
-      client.on("ready", () => {
-        console.log("ping.js a démarré")
-    });
-    
+client.on("ready", () => {
+    console.log("ping.js a démarré");
+});    
